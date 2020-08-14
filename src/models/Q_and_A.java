3222f8d0fbd514b6ca138e551a0d6263c502f1ef
A S Rakesh Krishna <rakeshkrishna517@gmail.com>
2020-08-14 13:01:52 +0530

    added option1

--- src/models/Q_and_A.java
+++ src/models/Q_and_A.java
@@ -3,9 +3,11 @@ package models;
 public class Q_and_A {
     public String question;
     public String answer;
+    public String option1;
 
     public Q_and_A(String q, String a) {
         question = q;
         answer = a;
+
     }
 }
